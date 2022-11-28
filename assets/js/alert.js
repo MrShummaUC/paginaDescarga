d0fb8a14da887c395242944614ab8a3d09c22236
Shumma <102603818+MrShummaUC@users.noreply.github.com>
2022-11-27 22:03:08 -0300

    creación página Descarga

---
+++ assets/js/alert.js
@@ -0,0 +1,9 @@
+function enviarCorreo(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Se ha enviado el correo',
+        text: "Puedes enviar mensajes cuando quieras!",
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
